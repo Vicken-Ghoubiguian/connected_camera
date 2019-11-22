c236214bebdefc896d6fdb2f70f386c81ad2264a
Eric GHOUBIGUIAN <eric.ghoubiguian@imerir.com>
2019-11-22 12:41:17 +0100

    Definition of a specified module for modes

--- src/connected_camera_main_module.py
+++ src/connected_camera_main_module.py
@@ -1,46 +1,10 @@
 import cv2
 import time
-import numpy as np
+import src.frame_mode_module as frame_mode_module
 import src.terminal_color_codes as terminal_color_codes
 import src.detection_module as detection_module
 import src.usefull_functions_module as usefull_functions_module
 from datetime import datetime
-
-def cartoonizing_image_function(img, ksize = 5, sketch_mode = False):
-
-	#Definition of variables with respectives values
-	num_repetitions, sigma_color, sigma_space, ds_factor = 10, 5, 7, 4
-
-	#Convert image to grayscale
-	img_gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-
-	#Apply median filter to the grayscale image
-	img_gray = cv2.medianBlur(img_gray, 7)
-
-	#Detect edges in the image and threshold it
-	edges = cv2.Laplacian(img_gray, cv2.CV_8U, ksize = ksize)
-	ret, mask = cv2.threshold(edges, 100, 255, cv2.THRESH_BINARY_INV)
-
-	#'mask' is the sketch of the image
-	if sketch_mode:
-
-		return cv2.cvtColor(mask, cv2.COLOR_GRAY2BGR)
-
-	#Resize the image to a smaller size for faster computation
-	img_small = cv2.resize(img, None, fx = 1.0/ds_factor, fy = 1.0/ds_factor, interpolation = cv2.INTER_AREA)
-
-	#Apply bilateral filter the image multiple times
-	for i in range(num_repetitions):
-		img_small = cv2.bilateralFilter(img_small, ksize, sigma_color, sigma_space)
-
-	img_output = cv2.resize(img_small, None, fx = ds_factor, fy = ds_factor, interpolation = cv2.INTER_LINEAR)
-
-	dst = np.zeros(img_gray.shape)
-
-	#Add the thick boundary lines to the image using 'AND' operator
-	dst = cv2.bitwise_and(img_output, img_output, mask = mask)
-
-	return dst
 
 def shoot_a_photo_function(title, desired_frame):
 
@@ -130,11 +94,11 @@ def exploits_function(title):
 
 		if is_current_mode == 'p':
 
-			frame = cartoonizing_image_function(frame, ksize = 5, sketch_mode = True)
+			frame = frame_mode_module.cartoonizing_image_function(frame, ksize = 5, sketch_mode = True)
 
 		elif is_current_mode == 'c':
 
-			frame = cartoonizing_image_function(frame, ksize = 5, sketch_mode = False)
+			frame = frame_mode_module.cartoonizing_image_function(frame, ksize = 5, sketch_mode = False)
 
 		else:
 
