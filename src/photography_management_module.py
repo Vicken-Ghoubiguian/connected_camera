9cbd4de2df9824b4c9c89cb606e71851816a5786
Eric GHOUBIGUIAN <eric.ghoubiguian@imerir.com>
2019-11-25 21:34:29 +0100

    Addition of sound effects

--- src/photography_management_module.py
+++ src/photography_management_module.py
@@ -1,5 +1,12 @@
 import cv2
+from pygame import mixer
 
-def shoot_a_photo_function(title, desired_frame, desired_format = '.jpg'):
+def shoot_a_photo_function(title, desired_frame, desired_format = '.jpg', desired_audio_file = '/home/eric/connected_camera/sounds/1_second_long_old_camera_sound.mp3'):
 
-        cv2.imwrite(title + desired_format, desired_frame)
+	mixer.init()
+
+	mixer.music.load(desired_audio_file)
+
+	mixer.music.play()
+
+	cv2.imwrite(title + desired_format, desired_frame)
