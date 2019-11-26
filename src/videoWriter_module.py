8611a9b461902c5dd17242053269c2f74d45e15d
Eric GHOUBIGUIAN <eric.ghoubiguian@imerir.com>
2019-11-26 07:42:48 +0100

    Adaptation for video shooting function

--- src/videoWriter_module.py
+++ src/videoWriter_module.py
@@ -1,10 +1,10 @@
 import cv2
 
-def initialisation_of_videoWriter_function(desired_title):
+def initialisation_of_videoWriter_function(desired_title, desired_format = '.mp4', desired_fourcc = 'MP4V'):
 
-        fourcc = cv2.VideoWriter_fourcc(*'MP4V')
+        fourcc = cv2.VideoWriter_fourcc(*desired_fourcc)
 
-        return cv2.VideoWriter(desired_title + '.mp4', fourcc, 20.0, (640,480))
+        return cv2.VideoWriter(desired_title + desired_format, fourcc, 20.0, (640,480))
 
 def writing_frame_function(desired_videoWriter, desired_frame):
 
