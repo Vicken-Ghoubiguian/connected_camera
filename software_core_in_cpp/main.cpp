1817c99ec0495a09b256e08ca87820bbd739c75a
Eric GHOUBIGUIAN <eric.ghoubiguian@imerir.com>
2019-12-16 07:46:41 +0100

    Beggining the structuration of cpp function exploits_function

--- software_core_in_cpp/main.cpp
+++ software_core_in_cpp/main.cpp
@@ -3,51 +3,85 @@
 #include <opencv2/videoio.hpp>
 #include <opencv2/highgui.hpp>
 
-void exploits_function(int mode)
-{
-    cv::VideoCapture cap;
-    cv::Mat frame;
+#include <iostream>
+#include <stdio.h>
 
-    cap.open(0); // open the default camera
+using namespace cv;
+using namespace std;
+
+void exploits_function()
+{
+    VideoCapture cap;
+    Mat frame;
+
+    int pressed_key;
+    int mode;
+
+    //Open the default camera
+    cap.open(0);
+
     if(!cap.isOpened()) {
-        // check if we succeeded
+
+	//
         return ;
     }
 
+    //
     for(;;)
     {
         cap.read(frame);
 
-        if(mode == 0)
-        {
-            cv::cvtColor(frame, frame, cv::COLOR_BGR2GRAY);
-            cv::Canny(frame, frame, 50, 150, 3);
-        }
-        else if(mode == 1)
-        {
-            cv::cvtColor(frame, frame, cv::COLOR_BGR2GRAY);
-        }
-        else if(mode == 2)
-        {
-            cv::medianBlur(frame, frame, 65);
-        }
-        else if(mode == 3)
-        {
-            cv::cvtColor(frame, frame, cv::COLOR_BGR2GRAY);
-            cv::HoughLinesP(frame, frame, 1, CV_PI/180, 70, 30, 10);
-        }
+	//
+	pressed_key = waitKey(1);
 
-        cv::imshow("Connected camera", frame);
+        if(pressed_key == 27) {
 
-        if(cv::waitKey(40) >= 0) {
             break;
+
         }
+        else if(pressed_key == 32) {
+
+            cout << "Pressed...\n";
+
+        }
+	else if((pressed_key >= 65 && pressed_key <= 90) || (pressed_key >= 97 && pressed_key <= 122)) {
+
+	    mode = pressed_key;
+
+	}
+
+	//
+        if(mode == 121)
+        {
+
+            cvtColor(frame, frame, COLOR_BGR2GRAY);
+            Canny(frame, frame, 50, 150, 3);
+
+        }
+        else if(mode == 119)
+        {
+
+            cvtColor(frame, frame, COLOR_BGR2GRAY);
+
+        }
+        else if(mode == 65)
+        {
+
+            medianBlur(frame, frame, 65);
+
+        }
+	else if(mode == 111)
+	{
+
+	}
+
+        imshow("Connected camera", frame);
     }
 }
 
 int main()
 {
-	exploits_function(4);
+	exploits_function();
 
 	return 0;
 }
