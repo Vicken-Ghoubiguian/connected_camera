ce9c6fee70ffde3c21f1e81008a57de75421ab3a
Eric GHOUBIGUIAN <eric.ghoubiguian@imerir.com>
2019-12-22 17:38:16 +0100

    Addition of modes enumeration

--- software_core_in_cpp/main.cpp
+++ software_core_in_cpp/main.cpp
@@ -1,8 +1,9 @@
 #include "src/exploits_on_gtk_library/exploits_on_gtk_library.hpp"
+#include "src/exploits_library/exploits_library.hpp"
 
 int main(int argc, char** argv)
 {
-	exploits_on_gtk_function(argc, argv);
+	exploits_function();
 
 	return 0;
 }
