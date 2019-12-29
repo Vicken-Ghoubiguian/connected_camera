682ec59c1179d815643f2f19a4f9a431fa82eed8
Eric GHOUBIGUIAN <eric.ghoubiguian@imerir.com>
2019-12-29 17:26:35 +0100

    Displaying all available modes in the Webcam displaying area

--- software_core_in_cpp/graphic_interface/Webcam_Displaying_Area/Webcam_Displaying_Area.cpp
+++ software_core_in_cpp/graphic_interface/Webcam_Displaying_Area/Webcam_Displaying_Area.cpp
@@ -1,8 +1,12 @@
 #include "Webcam_Displaying_Area.hpp"
 
+#include <iostream>
+
 #include <gdkmm/pixbuf.h>
 #include <gdkmm/general.h>
 #include <glibmm/main.h>
+
+using namespace std;
 
 Webcam_Displaying_Area::Webcam_Displaying_Area() : cv_opened(false)
 {
@@ -63,6 +67,30 @@ bool Webcam_Displaying_Area::on_draw(const Cairo::RefPtr<Cairo::Context> &cr)
 	if(current_mode == CC_BLACK_AND_WHITE_WITH_RGB_MODE)
 	{
 		cv_frame1 = black_and_white_frame_converting_function(cv_frame, COLOR_BGR2BGRA);
+	}
+	else if(current_mode == CC_GRAY_AND_WHITE_MODE)
+	{
+		cv_frame1 = gray_and_white_effect_function(cv_frame);
+	}
+	else if(current_mode == CC_BLACK_AND_WHITE_WITH_GRAY_MODE)
+	{
+		cv_frame1 = black_and_white_frame_converting_function(cv_frame, COLOR_BGR2GRAY);
+	}
+	else if(current_mode == CC_DRAW_MODE)
+	{
+		cv_frame1 = cartoonizing_image_function(cv_frame, 5, true);
+	}
+	else if(current_mode == CC_PAINTING_MODE)
+	{
+		cv_frame1 = cartoonizing_image_function(cv_frame, 5, false);
+	}
+	else if(current_mode == CC_NEGATIVE_OR_INVERTED_MODE)
+	{
+		cv_frame1 = negative_or_inverted_effect_function(cv_frame);
+	}
+	else if(current_mode == CC_HUE_SATURATION_LIGHTNESS_MODE)
+	{
+		cv_frame1 = hue_saturation_lightness_effect_function(cv_frame);
 	}
 	else if(current_mode == CC_EDGE_DETECTION_MODE)
 	{
